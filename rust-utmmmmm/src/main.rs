d555687296c56572a40bfe6e401c86cbcca1f356
Spencer Pearson <speeze.pearson@gmail.com>
2026-03-21 03:05:36 -0700

    fixup

--- rust-utmmmmm/src/main.rs
+++ rust-utmmmmm/src/main.rs
@@ -8,11 +8,20 @@ struct State(u8);
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Symbol(u8);
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct TuringMachineSpec {
     initial: State,
-    blank: Symbol,  // not actually used in the infinite-tape case, but the UTM *should* be able to 
+    blank: Symbol, // not actually used in the infinite-tape case, but the UTM *should* be able to
     accept: State,
-    transition_matrix: [Option<(State, Symbol, Dir)>; 1<<16],
+    transition_matrix: [Option<(State, Symbol, Dir)>; 1 << 16],
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct RunningTuringMachine {
+    spec: TuringMachineSpec,
+    state: State,
+    pos: usize,
+    tape: Vec<Symbol>,
 }
 
 fn build_utm_spec() -> TuringMachineSpec {
@@ -20,14 +29,16 @@ fn build_utm_spec() -> TuringMachineSpec {
     todo!()
 }
 
-
 fn build_header(spec: TuringMachineSpec, optimization_hints: Vec<(State, Symbol)>) -> Vec<Symbol> {
     // equivalent to the TypeScript `buildHeader(myUtmSpec, myUtmSpec.initial, optimizationHints)`
     todo!()
 }
 
 fn extend_infinite_utm_tape(tape: &mut Vec<Symbol>) {
-    let header = build_header(build_utm_spec(), todo!("see TypeScript infiniteUtmTapeBackground")); // ideally we would just compute this once
+    let header = build_header(
+        build_utm_spec(),
+        todo!("see TypeScript infiniteUtmTapeBackground"),
+    ); // ideally we would just compute this once, make it static or something
 
     // Equivalent to the TypeScript `infiniteUtmTapeBackground`.
     // Increase the tape's length by some substantial amount.
@@ -35,30 +46,54 @@ fn extend_infinite_utm_tape(tape: &mut Vec<Symbol>) {
     todo!()
 }
 
+fn step_turing_machine(
+    m: &mut RunningTuringMachine,
+    extend_tape: impl Fn(&mut Vec<Symbol>, Symbol),
+) -> bool {
+    while m.pos >= m.tape.len() {
+        extend_tape(&mut m.tape, m.spec.blank);
+    }
+    let symbol = m.tape[m.pos];
+    if let Some((next_state, next_symbol, dir)) =
+        m.spec.transition_matrix[((m.state.0 as usize) << 8) | (symbol.0 as usize)]
+    {
+        m.state = next_state;
+        m.tape[m.pos] = next_symbol;
+        m.pos = match dir {
+            Dir::Left => m.pos.saturating_sub(1),
+            Dir::Right => m.pos + 1,
+        };
+        true
+    } else {
+        false
+    }
+}
 
 fn main() {
     let spec = build_utm_spec();
-    let mut state = spec.initial;
-    let mut pos: usize = 0;
-    let mut tape: Vec<Symbol> = vec![];
+    let mut machine = RunningTuringMachine {
+        spec: spec.clone(),
+        state: spec.initial,
+        pos: 0,
+        tape: vec![],
+    };
     let mut steps = 0;
+    let extend_tape = |tape: &mut Vec<Symbol>, _blank: Symbol| {
+        extend_infinite_utm_tape(tape);
+    };
     loop {
         steps += 1;
-        while pos >= tape.len() {
-            extend_infinite_utm_tape(&mut tape);
-        }
-
-        let symbol = tape[pos];
-        if let Some((next_state, next_symbol, dir)) = spec.transition_matrix[((state.0 as usize) << 8) | (symbol.0 as usize)] {
-            state = next_state;
-            tape[pos] = next_symbol;
-            pos = match dir {
-                Dir::Left => pos.saturating_sub(1),
-                Dir::Right => pos + 1,
-            };
-        } else {
+        if !step_turing_machine(&mut machine, extend_tape) {
             break;
         }
     }
-    println!("halted in state {state:?} ({}) after {steps} steps", if state == spec.accept { "accept" } else { "reject" });
+    println!(
+        "halted in state {:?} ({}) after {steps} steps",
+        machine.state,
+        if machine.state == spec.accept {
+            "accept"
+        } else {
+            "reject"
+        }
+    );
 }
